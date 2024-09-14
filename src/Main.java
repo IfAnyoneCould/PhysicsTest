6a1c88ed7898b25d04cbf87f233a2f85da9f636e
Ashley Wood <ashleywood@ashleys-air.lan>
2024-09-13 18:05:58 -0600

    created a main loop that measures fps (thanks chatgpt)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    private static final int TARGET_FPS = 30;
+    private static final int TARGET_FPS = 60;
     private static final long OPTIMAL_TIME = 1000000000 / TARGET_FPS;
 
     public static void main(String[] args) {
@@ -8,17 +8,34 @@ public class Main {
         long lastLoopTime = System.nanoTime();
         long lastFrameTime;
 
+        // Variables for FPS calculation
+        int frames = 0;
+        long fpsTimeCounter = 0;
+        int realFPS = 0;
+
+
+
+
+
+
         while (true) {
             long now = System.nanoTime();
             long elapsedTime = now - lastLoopTime;
             lastLoopTime = now;
 
+            // Accumulate time for FPS calculation
+            fpsTimeCounter += elapsedTime;
+            frames++;
+
             double delta = elapsedTime / ((double) OPTIMAL_TIME);
 
             // do updates here
 
+
+
             // render here
-                System.out.println("FPS: " + delta);
+
+
             // sleep calculations
             lastFrameTime = System.nanoTime() - now;
             long sleepTime = (OPTIMAL_TIME - lastFrameTime) / 1000000;
@@ -29,6 +46,14 @@ public class Main {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+            }
+
+            //fps counter
+            if (fpsTimeCounter >= 1000000000) {
+                realFPS = frames;
+                //System.out.println("FPS: " + realFPS);
+                fpsTimeCounter = 0;
+                frames = 0;
             }
 
         }
